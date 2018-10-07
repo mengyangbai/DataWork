39f53939dbaf1f73820b47fbea3f93d14739bde0
mengyangbai <mengyangbainju@gmail.com>
2018-10-07 14:35:40 +1100

    save

--- RealEsate/craw_for_abs.py
+++ RealEsate/craw_for_abs.py
@@ -5,13 +5,20 @@
     @Author Bai
 '''
 import pandas as pd
-
+import requests
+from bs4 import BeautifulSoup
 source_data = "RealEsate/import_data.csv"
 simple_data_link = "http://quickstats.censusdata.abs.gov.au/census_services/getproduct/census/2016/quickstat/POA{}?opendocument"
-profile_link = "http://quickstats.censusdata.abs.gov.au/census_services/getproduct/census/2016/communityprofile/POA{}?opendocument"
+# profile_link = "http://quickstats.censusdata.abs.gov.au/census_services/getproduct/census/2016/communityprofile/POA{}?opendocument"
+
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+headers = { 'User-Agent' : user_agent}
 
 
 if __name__ == "__main__":
     import_area = pd.read_csv(source_data)
     for postcode in import_area['Postcode']:
-        print(simple_data_link.format(postcode))
+        if postcode == 2134:
+            r = requests.get(simple_data_link.format(postcode))
+            soup = BeautifulSoup(r.text,"xml")
+            print(soup.find("peopleContent"))
