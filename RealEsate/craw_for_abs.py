e0ba3766443373d6d69638fa79a74fd74df2ad29
Mengyang Bai <mengyang.b@exciteholidays.com>
2018-10-10 16:35:58 +1100

    Step by step

--- RealEsate/craw_for_abs.py
+++ RealEsate/craw_for_abs.py
@@ -16,13 +16,39 @@ simple_data_link = "http://quickstats.censusdata.abs.gov.au/census_services/getp
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
 headers = { 'User-Agent' : user_agent}
 
+'''
+var maritalStatusData = [];
+var ancestryParagraphs = [];
+var ancestryData = [];
+var religiousAffiliationParagraphs = [];
+var religiousAffiliationData = [];
+var languageParagraphs = [];
+var languageData = [];
+var occupationParagraphs = [];
+var occupationData = [];
+var countryOfBirthParagraphs = [];
+var countryOfBirthData = [];
+var travelToWorkParagraphs = [];
+var travelToWorkData = [];
+var industryParagraphs = [];
+var industryData = [];
+var countryOfFatherParagraphs = [];
+var countryOfFatherData = [];
+var countryOfMotherParagraphs = [];
+var countryOfMotherData = [];
+'''
+# Direct read the js data, is the fastest solution
 
 if __name__ == "__main__":
     import_area = pd.read_csv(source_data)
     for postcode in import_area['Postcode']:
-        if postcode == 2017:
+        if postcode == 2134:
             r = requests.get(simple_data_link.format(postcode))
             soup = BeautifulSoup(r.text,features="lxml")
             # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-            print(soup.find(id="mainContent"))
+            for sibling in soup.find(id="peopleContent").table.next_siblings:
+                # if sibling.string and "<!-- People -->" in sibling.string:
+                if sibling and sibling.table:
+                    print(sibling)
+                    print("---------")
             # print(soup.find_all(id="countryOfBirthTable"))
